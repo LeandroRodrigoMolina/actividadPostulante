d6a8e318094d51950fc66484db5c020dff86c514
Leandro Molina <LeandroRodrigoMolina@gmail.com>
2024-03-01 17:11:46 -0300

    Se agrego la eliminacion de subsidios

--- cliente/src/components/App/App.js
+++ cliente/src/components/App/App.js
@@ -1,17 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import SubsidioForm from "../Subsidio/SubsidioForm.js";
 import EliminarSubsidio from "../EliminarSubsidio/EliminarSubsidio.js";
+import axios from 'axios';
 
 const App = () => {
+    const [subsidios, setSubsidios] = useState([]);
+
+    // Función para cargar la lista de subsidios al montar el componente
+    useEffect(() => {
+        cargarSubsidios();
+    }, []);
+
+    // Función para cargar la lista de subsidios
+    const cargarSubsidios = async () => {
+        try {
+            const response = await axios.get('http://localhost:5000/subsidios');
+            setSubsidios(response.data);
+        } catch (error) {
+            console.error('Error al cargar subsidios:', error);
+        }
+    };
+
+    // Función para manejar la eliminación exitosa de un subsidio
     const handleEliminacionExitosa = () => {
-        // Código para manejar la eliminación exitosa del subsidio
-        console.log('Subsidio eliminado con éxito');
+        // Actualizar la lista de subsidios después de la eliminación
+        cargarSubsidios();
     };
 
     return (
         <div>
-            <SubsidioForm />
+            <SubsidioForm onSubsidioCreado={cargarSubsidios} />
             <EliminarSubsidio onEliminacionExitosa={handleEliminacionExitosa} />
         </div>
     );
--- cliente/src/components/EliminarSubsidio/EliminarSubsidio.js
+++ cliente/src/components/EliminarSubsidio/EliminarSubsidio.js
@@ -1,8 +1,22 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
 function EliminarSubsidio({ onEliminacionExitosa }) {
+    const [subsidios, setSubsidios] = useState([]);
     const [subsidioId, setSubsidioId] = useState('');
+
+    useEffect(() => {
+        const fetchSubsidios = async () => {
+            try {
+                const response = await axios.get('http://localhost:5000/subsidios');
+                setSubsidios(response.data);
+            } catch (error) {
+                console.error('Error al obtener los subsidios:', error);
+            }
+        };
+
+        fetchSubsidios();
+    }, []);
 
     const handleEliminarSubsidio = async () => {
         try {
@@ -18,12 +32,15 @@ function EliminarSubsidio({ onEliminacionExitosa }) {
 
     return (
         <div>
-            <input
-                type="text"
-                placeholder="ID del subsidio a eliminar"
+            <select
                 value={subsidioId}
                 onChange={(e) => setSubsidioId(e.target.value)}
-            />
+            >
+                <option value="">Seleccionar Subsidio a Eliminar</option>
+                {subsidios.map(subsidio => (
+                    <option key={subsidio.IdSubsidio} value={subsidio.IdSubsidio}>{subsidio.Descripcion}</option>
+                ))}
+            </select>
             <button onClick={handleEliminarSubsidio}>Eliminar Subsidio</button>
         </div>
     );
